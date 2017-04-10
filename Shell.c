50bd5070e620aa23fa3e23583f8eef02fd3a47b1
Joe Haddad <timer150@gmail.com>
2017-04-10 00:58:29 -0400

    Update shell

--- Shell.c
+++ Shell.c
@@ -1,24 +1,8 @@
 #include "blackdos.h"
 
-int str_length(char *c) {
-  int count = 0;
-  while (*c != '\0') {
-    ++count;
-    ++c;
-  }
-  return count;
-}
+int str_length(char *);
 
-int str_begins(char *str1, char *str2) {
-  int i;
-  int length;
-  length = str_length(str2);
-  for (i = 0; i < length; ++i) {
-    if (str1[i] != str2[i])
-      return 0;
-  }
-  return 1;
-}
+int str_begins(char *, char *);
 
 void main() {
   char command[255];
@@ -40,4 +24,24 @@ void main() {
       PRINTS("Unknown command. Type `help` for help.\r\n\0");
     }
   }
+}
+
+int str_length(char *c) {
+  int count = 0;
+  while (*c != '\0') {
+    ++count;
+    ++c;
+  }
+  return count;
+}
+
+int str_begins(char *str1, char *str2) {
+  int i;
+  int length;
+  length = str_length(str2);
+  for (i = 0; i < length; ++i) {
+    if (str1[i] != str2[i])
+      return 0;
+  }
+  return 1;
 }
